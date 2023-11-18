543b918f1dd9d2b15e8105b733cb444d15fcc9e4
Noga Gercsak <135066149+nogagercsak@users.noreply.github.com>
2023-11-18 18:55:00 -0500

    fixed code

---
+++ EcoEatsTracker1/EcoEatsTracker1App.swift
@@ -0,0 +1,17 @@
+//
+//  EcoEatsTracker1App.swift
+//  EcoEatsTracker1
+//
+//  Created by Noga Gercsak on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct EcoEatsTracker1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcoEatsTracker1/LogInView.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        VStack {
+            // Page header
+            VStack {
+                Text("welcome to")
+                    .font(Font.custom("SeoulHangang", size: 40))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.34, green: 0.41, blue: 0.34))
+                Text("EcoEats")
+                    .font(Font.custom("SeoulHangang CBL", size: 70))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.34, green: 0.41, blue: 0.34))
+                    .frame(width: 269, height: 78, alignment: .top)
+            }
+
+            // Sign up button
+            ZStack {
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: 233, height: 54)
+                  .background(Color(red: 0.43, green: 0.51, blue: 0.42))
+                  .cornerRadius(30)
+                Text("Sign up")
+                    .font(Font.custom("Radio Canada", size: 25))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.98, green: 0.93, blue: 0.66))
+                    .frame(width: 109.17875, height: 35.32394, alignment: .top)
+            }
+
+            // Log-in button
+            ZStack {
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: 233, height: 54)
+                  .background(Color(red: 1, green: 0.96, blue: 0.89))
+                  .cornerRadius(30)
+                  .border(Color(red: 0.427, green: 0.51, blue: 0.42))
+        
+                Text("Log in")
+                    .font(Font.custom("Radio Canada", size: 25))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.43, green: 0.51, blue: 0.42))
+                    .frame(width: 147, height: 35, alignment: .top)
+            }
+        
+
+            // Connect with Google button
+            ZStack {
+                Image("Group 6")
+                    .resizable()
+                    .frame(width: 233, height: 54)
+                Text("connect with google")
+                    .font(Font.custom("Radio Canada", size: 17))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.43, green: 0.51, blue: 0.42))
+                    .frame(width: 165, height: 18, alignment: .top)
+            }
+        }
+        .frame(width: 393, height: 852)
+        .background(Color(red: 1, green: 0.96, blue: 0.89))
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
