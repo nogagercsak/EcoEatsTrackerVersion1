36d4fc11883cddecda6c6ef87a11aaad4e18fbfb
Shradha <bista.shradha@gmail.com>
2023-11-23 11:40:02 -0500

    ProgressView.

--- EcoEatsTracker1/ProgressView.swift
+++ EcoEatsTracker1/ProgressView.swift
@@ -9,21 +9,62 @@ import SwiftUI
 
 struct ProgressView: View {
     var body: some View {
-        ZStack {
+        VStack {
             // Header
             Text("Progress:")
                 .font(Font.custom("Inter", size: 70))
                 .foregroundColor(.black)
-                .frame(width: 334, height: 94, alignment: .topLeading)
-        }
+                .frame(width: 334, height: 94, alignment: .top)
+            
+            ZStack {
+                Image("Ellipse 1")
+                    .frame(width: 300, height: 100)
+                    .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                Text("Food waste vs. Food saved")
+                    .font(Font.custom("Inter", size: 25))
+                    .foregroundColor(.black)
+                    .frame(width: 199, height: 90, alignment: .center)
+            } //ZStack line 19.
+            
+            ZStack {
+                Image("Ellipse 1")
+                    .frame(width: 300, height: 100)
+                    .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                Text("Top three food recipes:")
+                    .font(Font.custom("Inter", size: 25))
+                    .foregroundColor(.black)
+                    .frame(width: 199, height: 90, alignment: .center)
+            } //ZStack line 29.
+            
+            ZStack {
+                Image("Ellipse 1")
+                    .frame(width: 300, height: 100)
+                    .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                Text("How much food donated")
+                    .font(Font.custom("Inter", size: 25))
+                    .foregroundColor(.black)
+                    .frame(width: 199, height: 90, alignment: .center)
+            } //ZStack line 39.
+            
+            ZStack {
+                Image("Ellipse 1")
+                    .frame(width: 300, height: 100)
+                    .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                Text("My food bank")
+                    .font(Font.custom("Inter", size: 25))
+                    .foregroundColor(.black)
+                    .frame(width: 199, height: 90, alignment: .center)
+            } //ZStack line 49.
+            
+            //Nav Bar.
+        } //VStack line 12.
         .frame(width: 393, height: 852)
         .background(Color(red: 1, green: 0.96, blue: 0.89))
+        
     }
 }
-
 struct ProgressView_Previews: PreviewProvider {
     static var previews: some View {
         ProgressView()
     }
 }
-//This is a test run to see if my changes commit.
