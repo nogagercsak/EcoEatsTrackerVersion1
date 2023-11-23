981f22121aee8580210277df6857e07926b70892
Shradha <bista.shradha@gmail.com>
2023-11-23 11:56:56 -0500

    DonateView.

--- DonateView.swift
+++ DonateView.swift
@@ -13,10 +13,10 @@ struct DonateView: View {
                 .font(Font.custom("SeoulHangang CB", size: 40))
                 .foregroundColor(Color(red: 0.34, green: 0.41, blue: 0.34))
                 .frame(width: 231, height: 49, alignment: .top)
-            //align to middle
+            
             ZStack {
                 Image("Rectangle 13")
-                    .frame(width: 200, height: 50)
+                    .frame(width: 300, height: 50)
                     .background(Color(red: 0.62, green: 0.76, blue: 0.62))
                 Text("Donate")
                     .font(Font.custom("Inter", size: 25))
@@ -24,8 +24,27 @@ struct DonateView: View {
                     .frame(width: 306, height: 63, alignment: .center)
             } //ZStack line 17.
             
-            //Repeat lines 17 to 25 for each button. Buttons: Find a local food bank, and Learn about food waste
+            ZStack {
+                Image("Rectangle 13")
+                    .frame(width: 300, height: 50)
+                    .background(Color(red: 0.62, green: 0.76, blue: 0.62))
+                Text("Find a local food bank")
+                    .font(Font.custom("Inter", size: 25))
+                    .foregroundColor(.black)
+                    .frame(width: 306, height: 63, alignment: .center)
+            } //ZStack line 27.
             
+            ZStack {
+                Image("Rectangle 13")
+                    .frame(width: 300, height: 50)
+                    .background(Color(red: 0.62, green: 0.76, blue: 0.62))
+                Text("Learn about food waste")
+                    .font(Font.custom("Inter", size: 25))
+                    .foregroundColor(.black)
+                    .frame(width: 306, height: 63, alignment: .center)
+            } //ZStack line 37.
+          
+            //Nav Bar.
         } //VStack line 11.
         .frame(width: 393, height: 852)
         .background(Color(red: 1, green: 0.96, blue: 0.89))
